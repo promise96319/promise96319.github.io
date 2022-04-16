4663bbf7427d1adbb9169544b14dde1812c7ed62
promise96319 <1248975357@qq.com>
2022-04-17 01:43:04 +0800

    refactor: move vuepress to vitepress

---
+++ docs/.vitepress/navbar.ts
@@ -0,0 +1,107 @@
+
+
+console.log('11', 11)
+
+
+
+export const navbar = [
+  {
+    text: 'Vue源码解析',
+    items: [{ text: 'Vue源码解析', link: '/vue/' },
+    {
+      text: 'Vue3.2源码',
+      link: '/vue3/'
+    },
+    ]
+  },
+  {
+    text: 'js知识点',
+    link: '/javascript/'
+  },
+  {
+    text: 'React',
+    link: '/react/'
+  },
+  {
+    text: 'Webpack',
+    items: [
+      {
+        text: 'Webpack',
+        link: '/webpack/'
+      },
+      {
+        text: 'Rollup',
+        link: '/rollup/'
+      },
+      {
+        text: 'Vite',
+        link: '/vite/'
+      },
+      {
+        text: 'Babel',
+        link: '/babel/'
+      }
+    ]
+  },
+  {
+    text: 'Node',
+    link: '/node/'
+  },
+  {
+    text: '技术概览',
+    link: '/trend/'
+  },
+  {
+    text: '算法',
+    items: [
+      {
+        text: '算法',
+        link: '/algorithms/'
+      },
+      {
+        text: 'leetcode',
+        link: '/leetcode/'
+      },
+      {
+        text: '数据结构',
+        link: '/data-structures/'
+      },
+      {
+        text: '设计模式',
+        link: '/design-pattern/'
+      }
+    ]
+  },
+  {
+    text: '网络',
+    link: '/network/'
+  },
+  {
+    text: '前端资源',
+    link: '/resources/'
+  },
+  {
+    text: '工具',
+    link: '/tools/'
+  },
+  {
+    text: '项目问题',
+    link: '/project/'
+  },
+  {
+    text: 'studying',
+    link: '/studying/'
+  }
+  // {
+  //   text: 'linux',
+  //   link: '/linux/'
+  // },
+  // {
+  //   text: '其他知识点',
+  //   link: 'knowledge-points'
+  // }
+  // {
+  //   text: '关于我',
+  //   link: '/about/'
+  // }
+]
---
+++ docs/index.md
@@ -0,0 +1,33 @@
+---
+home: true
+heroImage: /logo.png
+heroText: promise96319
+actionText: 笔记
+actionLink: /vue/
+# navbar: false
+features:
+- title: 前端框架
+  details: 学习前端框架代码，如 Vue、React。
+- title: 构建工具
+  details: 学习构建工具，如 Webpack、Rollup、Gulp、Rollup、Esbuild、Babel 等。
+- title: 开源贡献
+  details: 学习开源项目，思考解决遇到的问题。
+---
+
+<div class="footer" align="center">
+  <a href="https://beian.miit.gov.cn/">
+    鄂ICP备20002086号-1
+  </a>
+</div>
+
+<style>
+.tagline {
+  display: none;
+}
+
+.footer a {
+  padding: 24px 0;
+  color: #4e6e8e;
+  font-weight: 400;
+}
+</style>
