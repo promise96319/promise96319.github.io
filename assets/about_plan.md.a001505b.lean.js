00cd1f0d434d35595c950a0faf344e630a8ce0fb
promise96319 <1248975357@qq.com>
2022-05-10 15:19:21 +0000

    promise96319 published a site update

---
+++ assets/about_plan.md.a001505b.lean.js
@@ -0,0 +1,1 @@
+import{_ as l,c as i,o as t,a as e}from"./app.d322b2e6.js";const b='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"about/plan.md","lastUpdated":1652195878000}',a={},_=e("",2),n=[_];function u(o,p,s,r,c,d){return t(),i("div",null,n)}var f=l(a,[["render",u]]);export{b as __pageData,f as default};
--- assets/linux_index.md.94ca41a2.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,o as t,a as i}from"./app.d322b2e6.js";const m='{"title":"linux","description":"","frontmatter":{},"headers":[],"relativePath":"linux/index.md","lastUpdated":1652195166000}',s={},r=i("",2),_=[r];function l(n,o,h,c,d,u){return t(),e("div",null,_)}var p=a(s,[["render",l]]);export{m as __pageData,p as default};
