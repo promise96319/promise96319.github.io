76025100fa601010372d1ce7b14f59e7a326f1ad
promise96319 <38751863+promise96319@users.noreply.github.com>
2022-08-17 01:10:54 +0000

    Deploying to gh-pages from @ promise96319/promise96319.github.io@7cdc16c6d28d2d00f566faedc66c3b4e2159ed86 🚀

--- assets/linux_index.md.9a7f6faa.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as a,o as t,a as i}from"./app.6d355226.js";const x=JSON.parse('{"title":"linux","description":"","frontmatter":{},"headers":[],"relativePath":"linux/index.md","lastUpdated":1660527605000}'),n={name:"linux/index.md"},s=i("",2),r=[s];function l(_,d,o,h,c,u){return t(),a("div",null,r)}var p=e(n,[["render",l]]);export{x as __pageData,p as default};
