ba1ff4dbe59e4e3c61898c9d15ea1e6f300571fe
promise96319 <1248975357@qq.com>
2023-12-05 20:38:43 +0800

    feat: lint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  ignores: [
+    'node_modules',
+    '**/node_modules/**',
+    'dist',
+    '**/dist/**',
+
+    // TODO: 暂时禁用 markdown 里的一些问题
+    'docs/vue/**',
+    'docs/leetcode/**',
+  ],
+  vue: true,
+  markdown: true,
+  jsx: true,
+})
